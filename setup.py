d51576ad76c2eee3f4ab3977e52365e81bc10020
CSopiko <skurd18@freeuni.edu.ge>
2022-05-09 21:12:10 +0400

    feat: initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This is the ai/ml part of the organization ts-ai-kitkhe',
+    author='CSopiko',
+    license='MIT',
+)
