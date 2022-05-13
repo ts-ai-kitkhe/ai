e20a6f3d0882d22e958628921eab1972b89de226
CSopiko <skurd18@freeuni.edu.ge>
2022-05-13 14:43:56 +0400

    feat(utils): Add frontend utils

--- src/utils/frontend_utils.py
+++ src/utils/frontend_utils.py
@@ -2,7 +2,7 @@ from typing import List
 
 
 def input_for_frontend(corners: List[List[int]], json_path: str) -> None:
-    '''
+    """
     function generates json on the given path for frontend usage, each corner having its id as string.
     example: {"id_0": [[0, 1],[0, 3],[2, 0],[1, 0]]}
 
@@ -14,8 +14,9 @@ def input_for_frontend(corners: List[List[int]], json_path: str) -> None:
     Returns
     -----
     None
-    '''
+    """
     import json
+
     id_corners = {str(i_d): corners[i_d] for i_d in range(len(corners))}
     with open(json_path, "w") as f:
         json.dump(id_corners, f)
