04ad77980a5d44e88c7fded3dea38362d7ccd3f1
CSopiko <skurd18@freeuni.edu.ge>
2022-06-04 21:45:48 +0400

    feat(utils): zero padding

--- src/utils/data_utils.py
+++ src/utils/data_utils.py
@@ -1,7 +1,10 @@
-from typing import List
+from typing import List, Tuple
 
+import cv2
 import numpy as np
 import numpy.typing
+
+from src.utils.constant_utils import DEFAULT_MODEL_INPUT_SHAPE
 
 
 def get_characters(
@@ -29,3 +32,55 @@ def get_characters(
             ]
         )
     return characters
+
+
+def zero_padding(
+    binary_image: np.typing.NDArray[np.uint8],
+    desired_shape: Tuple[int, int] = DEFAULT_MODEL_INPUT_SHAPE,
+    pad_value: int = 0,
+) -> np.typing.NDArray[np.uint8]:
+    """
+    function pads given image with desired padding value to desired shape
+
+    Parameters
+    -----
+    binary_image: np.typing.NDArray[np.uint8]
+    desired_shape: Tuple[int, int]
+    pad_value: int
+
+    Returns
+    np.typing.NDArray[np.uint8]
+    -----     
+    """
+    # if any side > desired shape -> scaling
+    scale = 1
+    if (
+        binary_image.shape[0] > desired_shape[0]
+        and binary_image.shape[1] > desired_shape[1]
+    ):
+        if binary_image.shape[0] > binary_image.shape[1]:
+            scale = desired_shape[0] / binary_image.shape[0]
+        else:
+            scale = desired_shape[1] / binary_image.shape[1]
+    elif binary_image.shape[0] > desired_shape[0]:
+        scale = desired_shape[0] / binary_image.shape[0]
+
+    elif binary_image.shape[1] > desired_shape[1]:
+        scale = desired_shape[1] / binary_image.shape[1]
+
+    width = int(binary_image.shape[1] * scale)
+    height = int(binary_image.shape[0] * scale)
+    dim = (width, height)
+    resized = cv2.resize(binary_image, dim)
+    pad = np.full(desired_shape, np.uint8(pad_value))
+    # if curr shape < desired shape -> zero padding
+    if (
+        resized.shape[0] <= desired_shape[0]
+        and binary_image.shape[1] <= desired_shape[1]
+    ):
+        l = (pad.shape[0] - resized.shape[0]) // 2
+        u = (pad.shape[1] - resized.shape[1]) // 2
+        pad[l : resized.shape[0] + l, u : resized.shape[1] + u] = resized
+    else:
+        pad = binary_image[: desired_shape[0], : desired_shape[1]]
+    return pad
