ae0dc046e4a58ed1035e591e556a7c4913569138
CSopiko <skurd18@freeuni.edu.ge>
2022-07-09 17:22:41 +0400

    feat(utils)

--- src/utils/data_utils.py
+++ src/utils/data_utils.py
@@ -49,8 +49,8 @@ def zero_padding(
     pad_value: int
 
     Returns
+    -----
     np.typing.NDArray[np.uint8]
-    -----     
     """
     # if any side > desired shape -> scaling
     scale = 1
@@ -82,5 +82,61 @@ def zero_padding(
         u = (pad.shape[1] - resized.shape[1]) // 2
         pad[l : resized.shape[0] + l, u : resized.shape[1] + u] = resized
     else:
-        pad = binary_image[: desired_shape[0], : desired_shape[1]]
+        pad[
+            : min(desired_shape[0], binary_image.shape[0]),
+            : min(desired_shape[1], binary_image.shape[1]),
+        ] = binary_image[: desired_shape[0], : desired_shape[1]]
     return pad
+
+
+def remove_extra_space_around_characters(
+    binary_image: np.typing.NDArray[np.uint8], extra_space_value: int = 255
+) -> np.typing.NDArray[np.uint8]:
+    """
+    function removes extra space filled with some arbitrary value around characters in binary image
+
+    Parameters
+    -----
+    binary_image: np.typing.NDArray[np.uint8]
+    extra_space_value: int
+
+    Returns
+    -----
+    np.typing.NDArray[np.uint8]
+    """
+
+    n_rows, n_cols = binary_image.shape
+    upper_row, lower_row, left_col, right_col = 0, n_rows, 0, n_cols
+
+    for i in range(n_rows):
+        if sum(binary_image[i, :]) == n_cols * extra_space_value:
+            upper_row = i
+        else:
+            break
+
+    for i in range(n_rows):
+        if sum(binary_image[n_rows - i - 1, :]) == n_cols * extra_space_value:
+            lower_row = n_rows - i - 1
+        else:
+            break
+
+    for j in range(n_cols):
+        if sum(binary_image[:, j]) == n_rows * extra_space_value:
+            left_col = j
+        else:
+            break
+
+    for j in range(n_cols):
+        if sum(binary_image[:, n_cols - j - 1]) == n_rows * extra_space_value:
+            right_col = n_cols - j - 1
+        else:
+            break
+    removed_extra_space_image = binary_image[
+        upper_row : lower_row + 1, left_col : right_col + 1
+    ]
+    if (
+        removed_extra_space_image.shape[0] == 0
+        or removed_extra_space_image.shape[1] == 1
+    ):
+        return binary_image
+    return removed_extra_space_image
